9eaec0e34e9bcb33ffdd8ee14e23f5471c77a5bb
LuisRo BS <lrobreso@gmail.com>
2016-08-20 12:19:17 -0600

    Limpieza de Codigo

--- Door/Door.ino
+++ Door/Door.ino
@@ -6,10 +6,7 @@
 
 #include <RFM69.h>    //get it here: https://www.github.com/lowpowerlab/rfm69
 #include <SPI.h>
-#include <SPIFlash.h> //get it here: https://www.github.com/lowpowerlab/spiflash
 #include "LowPower.h"
-
-
 
 #define NODEID        2    //unique for each node on same network
 #define NETWORKID     100  //the same on all nodes that talk to each other
@@ -17,95 +14,82 @@
 //Match frequency to the hardware version of the radio on your Moteino (uncomment one):
 #define FREQUENCY   RF69_433MHZ
 #define ENCRYPTKEY    "sampleEncryptKey" //exactly the same 16 characters/bytes on all nodes!
-#define IS_RFM69HW    //uncomment only for RFM69HW! Leave out if you have RFM69W!
 #define ACK_TIME      30 // max # of ms to wait for an ack
 #define LED           9 // Moteinos have LEDs on D9
-#define FLASH_SS      8 // and FLASH SS on D8
-
 #define SERIAL_BAUD   115200
 
 
 // Use pin 2 as wake up pin
-const int wakeUpPin = 2;
-
-
-int TRANSMITPERIOD = 500; //transmit a packet to gateway so often (in ms)
+const int wakeUpPin = 3;
+int pin = 0;
+int door=0;
 
 char paquete;
-char buff[20];
 byte sendSize=1;
-boolean requestACK = false;
 RFM69 radio;
-int door=0;
 
 void wakeUp()
 {
     // Just a handler for the pin interrupt.
+    //Serial.println("INT");
+    //Serial.flush();
+
+    //digitalWrite(LED, pin);
+    
+    //if(pin) pin=0;
+    //else pin = 1;
+    
+    sendPackage();
 }
 
-void setup() {
-  Serial.begin(SERIAL_BAUD);
-  Serial.println("0");
-
+void setup(){
+  
+  //pinMode(LED, OUTPUT);
+  pinMode(wakeUpPin, INPUT); 
+  
+  //Serial.begin(SERIAL_BAUD);
   radio.initialize(FREQUENCY,NODEID,NETWORKID);
-  radio.setHighPower(); //uncomment only for RFM69HW!
+  radio.setHighPower();
   radio.encrypt(ENCRYPTKEY);
-  Serial.println("0.1");
-  pinMode(7,INPUT);
-  pinMode(wakeUpPin, INPUT);   
-  delay(100);
-
+    
+  //Serial.println("Starting...");
+  //Serial.flush();
 }
 
 void loop() {
   
-  Serial.println("1");
   // Allow wake up pin to trigger interrupt on low.
-    attachInterrupt(wakeUpPin, wakeUp, CHANGE);
-  Serial.println("2");  
+    attachInterrupt(1, wakeUp, CHANGE);
     // Enter power down state with ADC and BOD module disabled.
     // Wake up when wake up pin is low.
+    //Serial.flush();
     LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
-    Serial.println("3");
     // Disable external pin interrupt on wake up pin.
-    detachInterrupt(wakeUpPin); 
-    Serial.println("4");
+    detachInterrupt(1); 
     // Do something here
     // Example: Read sensor, data logging, data transmission.
 
-    sendPackage();
-    Blink(LED,3);
-  
-}
-
-void Blink(byte PIN, int DELAY_MS)
-{
-  pinMode(PIN, OUTPUT);
-  digitalWrite(PIN,HIGH);
-  delay(DELAY_MS);
-  digitalWrite(PIN,LOW);
 }
 
 void sendPackage(){
   
+  delay(10);
   door = digitalRead(7);
+  
+  if(door) paquete= 'c';
+  else paquete = 'o';
+  
+  /*Serial.print("Door: ");
+  Serial.println(door);
     
-    if(door) paquete= 'o';
-    else paquete = 'c';
-    
-    
-    Serial.print("Door: ");
-    Serial.println(door);
-      
-    Serial.print("Sending[");
-    Serial.print(sendSize);
-    Serial.print("]: ");
-
-
-    if (radio.sendWithRetry(GATEWAYID, &paquete, sizeof(paquete)))
-     Serial.print(" ok!");
-    else Serial.print(" nothing...");
-    
-    Serial.println();
+  Serial.print("Sending[");
+  Serial.print(sendSize);
+  Serial.print("]: ");*/
+  radio.sendWithRetry(GATEWAYID, &paquete, sizeof(paquete));
+  //if (radio.sendWithRetry(GATEWAYID, &paquete, sizeof(paquete)))
+   //Serial.print(" ok!");
+  //else Serial.print(" nothing...");
+  
+  //Serial.println();
   
 }
