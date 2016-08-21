fae0e18bc3c4863f88f111724210a85a063b56d7
LuisRo BS <lrobreso@gmail.com>
2016-08-21 09:04:40 -0600

    Receptor Working

--- Receptor/Receptor.ino
+++ Receptor/Receptor.ino
@@ -6,37 +6,27 @@
 
 #include <RFM69.h>    //get it here: https://www.github.com/lowpowerlab/rfm69
 #include <SPI.h>
-#include <SPIFlash.h> //get it here: https://www.github.com/lowpowerlab/spiflash
 
 #define NODEID        1    //unique for each node on same network
 #define NETWORKID     100  //the same on all nodes that talk to each other
-//Match frequency to the hardware version of the radio on your Moteino (uncomment one):
 #define FREQUENCY     RF69_433MHZ
-//#define FREQUENCY     RF69_868MHZ
-//#define FREQUENCY     RF69_915MHZ
 #define ENCRYPTKEY    "sampleEncryptKey" //exactly the same 16 characters/bytes on all nodes!
-#define IS_RFM69HW    //uncomment only for RFM69HW! Leave out if you have RFM69W!
 #define ACK_TIME      30 // max # of ms to wait for an ack
 #define SERIAL_BAUD   115200
 #define LED           9 // Moteinos have LEDs on D9
 
 RFM69 radio;
-
-struct payload {
-  int door;
-};
-
-payload *package;
+char message[8];
 
 void setup() {
-  Serial.begin(SERIAL_BAUD);
-  delay(10);
+  //Serial.begin(SERIAL_BAUD);
   radio.initialize(FREQUENCY,NODEID,NETWORKID);
-  radio.setHighPower(); //only for RFM69HW!
+  radio.setHighPower();
   radio.encrypt(ENCRYPTKEY);
-  char buff[50];
+  /*char buff[50];
   sprintf(buff, "\nListening at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
-  Serial.println(buff);
+  Serial.println(buff);*/
+  pinMode(9, OUTPUT);
 }
 
 byte ackCount=0;
@@ -45,35 +35,30 @@ void loop() {
 
   if (radio.receiveDone())
   {
-    Serial.print("#[");
+    /*Serial.print("#[");
     Serial.print(++packetCount);
     Serial.print(']');
-    Serial.print('[');Serial.print(radio.SENDERID, DEC);Serial.print("] ");
+    Serial.print('[');Serial.print(radio.SENDERID, DEC);Serial.print("] ");*/
 
-    for (byte i = 0; i < radio.DATALEN; i++)
-      Serial.print((char)radio.DATA[i]);
-    Serial.print("   [RX_RSSI:");Serial.print(radio.RSSI);Serial.print("]");
+    for (byte i = 0; i < radio.DATALEN; i++){
+      message[i] = (char)radio.DATA[i];
+      //Serial.print(message[i]);
+    }
+    //Serial.print("   [RX_RSSI:");Serial.print(radio.RSSI);Serial.print("]");
     
-    package = radio.DATA;
-    
-    Serial.print("A=");
-    Serial.println(package.door);
     
     if (radio.ACKRequested())
     {
       byte theNodeID = radio.SENDERID;
       radio.sendACK();
-      Serial.print(" - ACK sent.");                
+      //Serial.print(" - ACK sent.");                
     }
-    Serial.println();
-    Blink(LED,3);
-  }
-}
-
-void Blink(byte PIN, int DELAY_MS)
-{
-  pinMode(PIN, OUTPUT);
-  digitalWrite(PIN,HIGH);
-  delay(DELAY_MS);
-  digitalWrite(PIN,LOW);
+    //Serial.println();
+    
+    /*Serial.print("Message[0]=");
+    Serial.println(message[0]);*/
+    
+    if(message[0] == 'c') digitalWrite(9, HIGH);
+    else digitalWrite(9, LOW);
+  } 
 }
